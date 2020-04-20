5c412e6caa97a9633db4007fff31cc2be5054332
jensenal27 <48623977+jensenal27@users.noreply.github.com>
2020-04-20 16:20:42 -0400

    mailer update

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.post('/send', (req, res) => {
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // true for 465, false for other ports
+        host: 'smtp.mailgun.org',
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.USER, // generated ethereal user
             pass: process.env.PASS // generated ethereal password
